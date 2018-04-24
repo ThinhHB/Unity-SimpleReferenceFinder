1cea648126577cb7af29f3c3bec486614a00ce95
ThinhHB <hbthinh176177@gmail.com>
2018-04-24 16:46:46 +0700

    - Create the sample for the tool

---
+++ Assets/Sample/ReferenceHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Just a component that hold reference to another object on scene
+/// </summary>
+public class ReferenceHolder : MonoBehaviour {
+	[SerializeField] GameObject gameobjectReference;
+	[SerializeField] Rigidbody2D rigidbodyReference;
+	[SerializeField] ReferenceHolder monobehaviorReference;
+	[SerializeField] GameObject[] referenceArray;
+}
